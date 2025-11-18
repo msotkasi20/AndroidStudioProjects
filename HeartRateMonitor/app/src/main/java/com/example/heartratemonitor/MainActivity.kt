d07249a574206ef5dd4aa45fdf22e4a8945e380c
msotkasi20 <msotkasi20@gmail.com>
2025-11-18 14:32:04 +0200

    Heart Rate Limits done

--- HeartRateMonitor/app/src/main/java/com/example/heartratemonitor/MainActivity.kt
+++ HeartRateMonitor/app/src/main/java/com/example/heartratemonitor/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.heartratemonitor.ui.theme.HeartRateMonitorTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +32,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HeartRateMonitorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    HeartRateLimits(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +42,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HeartRateLimits(modifier: Modifier = Modifier) {
+    var ageInput by remember { mutableStateOf("") }
+    val age = ageInput.toIntOrNull() ?: 0
+    val upper = if (age > 0) (220 - age) * 0.85f else 0.0f
+    val lower = if (age > 0) (220 - age) * 0.65f else 0.0f
+    val upperFormatted = String.format("%.2f", upper)
+    val lowerFormatted = String.format("%.2f", lower)
+
+
+    Column (
+        modifier = modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        TextField(
+            value = ageInput,
+            onValueChange={ageInput = it},
+            label = {Text(text= stringResource(R.string.age))},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = stringResource(R.string.your_heart_rate_limits_are,
+                lowerFormatted,upperFormatted),
+            modifier = modifier
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HeartRateLimitsPreview() {
     HeartRateMonitorTheme {
-        Greeting("Android")
+        HeartRateLimits()
     }
 }
