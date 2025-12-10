f371b1aa420944f41e6ab5d6e908d7ab0278d4ab
msotkasi20 <msotkasi20@gmail.com>
2025-12-10 20:57:23 +0200

    MVVM tehty

--- MVVM/app/src/main/java/com/example/mvvm/MainActivity.kt
+++ MVVM/app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -4,15 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mvvm.ui.theme.MVVMTheme
+import com.example.mvvm.viewmodels.MyViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,8 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MVVMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MyComponent(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,6 +40,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyComponent() {
-    var count by remember {mutableIntStateOf(0)}
+fun MyComponent(modifier: Modifier = Modifier, myViewModel: MyViewModel = viewModel()) {
+    Column (
+        modifier = modifier.padding(16.dp)
+    ) {
+        Button(
+            onClick = { myViewModel.increment() },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Count: ${myViewModel.count}")
+        }
+    }
 }
